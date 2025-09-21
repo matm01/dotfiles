1f3fd570406988a11b6ba5b4d78d6cc3cfa0ee8e
matm01 <mumm.mathias@gmail.com>
2025-09-21 20:30:41 +0200

    add keymapps

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -11,7 +11,7 @@ return {
       ensure_installed = {
         -- install language servers
         "lua-language-server",
-        "pyright",
+        "pyrefly",
 
         -- install formatters
         "stylua",
