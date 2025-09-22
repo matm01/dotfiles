170e21b8dfcb606fca136a5a4e497dd161e9b47a
matm <mumm.mathias@gmail.com>
2025-09-22 09:25:40 +0200

    fix .zshrc

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -16,11 +16,10 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Move lines up/down
-vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", desc = "Move line down")
-vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", desc = "Move line up")
-vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", desc = "Move selection down")
-vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", desc = "Move selection up")
-
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 
 -- validate that lazy is available
 if not pcall(require, "lazy") then
